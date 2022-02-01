3c66059e319491ed51de771b00ee3791d06c5c5f
rachel kaip <kaipr@mail.gvsu.edu>
2022-02-01 07:56:11 -0500

    adding work up to 7.3.1

--- schema.sql
+++ schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE dept_manager (
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCE employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCE departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE salaries (
@@ -40,12 +40,15 @@ CREATE TABLE dept_employees (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+DROP TABLE IF EXISTS titles;
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, from_date)
 );
 SELECT * FROM departments;
+SELECT * FROM dept_manager
+SELECT * FROM employees
